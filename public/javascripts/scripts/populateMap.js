d8c706368dc26f40e71047a906e5226fa494ca0b
federicofrontera <federicofrontera@gmail.com>
2020-08-04 20:53:32 -0300

    refactor map stuff to a script

---
+++ public/javascripts/scripts/populateMap.js
@@ -0,0 +1,25 @@
+const coordinates = document.querySelector('#map').textContent.split(',');
+const name = document.querySelector('#campground-name').textContent;
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoib2tvbmRvciIsImEiOiJja2RnMmhrYmcyZHc4MnFwbTI5OXljdzBsIn0.FvyZoJXrxisObGhcQYpg6A';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center:  coordinates,
+    zoom: 10
+});
+
+// add markers to map
+
+// create a HTML element for each feature
+var el = document.createElement('div');
+el.className = 'marker';
+
+// make a marker for each feature and add to the map
+new mapboxgl.Marker(el)
+    .setLngLat(coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML('<h3>' + name + '</h3><p>' + coordinates + '</p>'))
+    .addTo(map);
